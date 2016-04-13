2d07ab01e460e98c2ba7731c303a5a9847a4084c
Brian Jessee <info@jesseeproductions.com>
2016-04-13 08:53:02 -0400

    Initial Commit 2.2.5

--- coupon_creator.php
+++ coupon_creator.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Coupon Creator
 Description: This plugin creates a custom post type for coupons with a shortcode to display it on website and a single view template for printing.
-Version: 2.2.2.1
+Version: 2.2.5
 Author: Brian Jessee
 Author URI: http://couponcreatorplugin.com
 Text Domain: coupon-creator
@@ -21,7 +21,7 @@ if (!defined('CCTOR_URL'))				define( 'CCTOR_URL',	plugin_dir_url( __FILE__ ));
 if (!defined('CCTOR_MIN_PHP_VERSION'))	define( 'CCTOR_MIN_PHP_VERSION',	'5.2');
 if (!defined('CCTOR_MIN_WP_VERSION'))	define( 'CCTOR_MIN_WP_VERSION',		'4.0');
 if (!defined('CCTOR_VERSION_KEY')) 		define( 'CCTOR_VERSION_KEY', 	'cctor_coupon_version');
-if (!defined('CCTOR_VERSION_NUM'))  	define( 'CCTOR_VERSION_NUM', 	'2.2.2.1');
+if (!defined('CCTOR_VERSION_NUM'))  	define( 'CCTOR_VERSION_NUM', 	'2.2.5');
 
 /*
 * Coupon Creator License
