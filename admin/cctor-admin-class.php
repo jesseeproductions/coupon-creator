23c8d485a7aaa6a7a1c297ab1bd88bd24b149c49
Brian Jessee <info@jesseeproductions.com>
2016-05-17 09:27:40 -0400

    Move Admin Columns and Update Methods to their own classes

--- admin/cctor-admin-class.php
+++ admin/cctor-admin-class.php
@@ -24,8 +24,8 @@ if ( ! class_exists( 'Coupon_Creator_Plugin_Admin' ) ) {
 			//Flush Permalinks on Permalink Option Change
 			add_action('admin_init', array( __CLASS__, 'cctor_flush_permalinks'));
 
-			// Remove Coupon Row Actions
-			add_filter( 'post_row_actions',  array( __CLASS__, 'cctor_remove_coupon_row_actions'), 10, 2 );
+			Coupon_Creator_Plugin::include_file( 'admin/cctor-admin-columns.php' );
+			new Coupon_Admin_Columns();
 
 			//Load Coupon Options Class
 			Coupon_Creator_Plugin::include_file( 'admin/cctor-options-class.php' );
@@ -59,12 +59,6 @@ if ( ! class_exists( 'Coupon_Creator_Plugin_Admin' ) ) {
 			//Load Admin Coupon Scripts
 			add_action( 'admin_enqueue_scripts', array( __CLASS__, 'cctor_edit_enqueue_style_scripts' ) );
 
-			// Add Columns
-			add_filter( 'manage_edit-cctor_coupon_columns' ,  array( __CLASS__, 'cctor_list_columns' ) );
-
-			//Custom Column Cases
-			add_action( 'manage_posts_custom_column', array( __CLASS__, 'cctor_column_cases' ), 10, 2 );
-
 		} //end admin_init
 	/***************************************************************************/
 
@@ -79,136 +73,14 @@ if ( ! class_exists( 'Coupon_Creator_Plugin_Admin' ) ) {
 
 			update_option( 'coupon_update_version', date('l jS \of F Y h:i:s A') );
 
-			self::cctor_update_expiration_option();
-
-			//self::cctor_update_ignore_expiration();
-
-			self::cctor_update_image_fields();
+			Coupon_Creator_Plugin::include_file( 'admin/cctor-update-scripts.php' );
+			new Coupon_Update_Scripts();
 
 			update_option(CCTOR_VERSION_KEY, CCTOR_VERSION_NUM);
 
 			update_option( 'cctor_coupon_base_change', TRUE );
 
 		}
-	}
-
-	/***************************************************************************/
-		/**
-		 * Update Coupons with new Expiration Options in 2.3
-		 */
-		public static function cctor_update_expiration_option() {
-			//Run this script once
-			if ( get_option( 'coupon_update_expiration_type' ) ) {
-				return;
-			}
-			$args = array(
-				'posts_per_page' => 1000,
-				'post_type'      => 'cctor_coupon',
-				'post_status'    => 'publish',
-			);
-
-			$cctor_exp_option = new WP_Query( $args );
-
-			if ( $cctor_exp_option ) {
-
-				while ( $cctor_exp_option->have_posts() ) : $cctor_exp_option->the_post();
-
-					//If there is an Expiration Option Skip this Coupon
-					$cctor_expiration_option          = get_post_meta( $cctor_exp_option->post->ID, 'cctor_expiration_option', true );
-					if ( $cctor_expiration_option ) {
-						continue;
-					}
-
-					$cctor_ignore_expiration          = get_post_meta( $cctor_exp_option->post->ID, 'cctor_ignore_expiration', true );
-					$cctor_expiration                 = get_post_meta( $cctor_exp_option->post->ID, 'cctor_expiration', true );
-					$cctor_recurring_expiration_limit = get_post_meta( $cctor_exp_option->post->ID, 'cctor_recurring_expiration_limit', true );
-					$cctor_recurring_expiration       = get_post_meta( $cctor_exp_option->post->ID, 'cctor_recurring_expiration', true );
-					$cctor_x_days_expiration          = get_post_meta( $cctor_exp_option->post->ID, 'cctor_x_days_expiration', true );
-
-					if ( 1 == $cctor_ignore_expiration ) {
-						update_post_meta( $cctor_exp_option->post->ID, 'cctor_expiration_option', 1 );
-					} elseif ( $cctor_expiration && ! $cctor_recurring_expiration_limit && ! $cctor_recurring_expiration ) {
-						update_post_meta( $cctor_exp_option->post->ID, 'cctor_expiration_option', 2 );
-					} elseif ( $cctor_expiration && $cctor_recurring_expiration_limit && $cctor_recurring_expiration ) {
-						update_post_meta( $cctor_exp_option->post->ID, 'cctor_expiration_option', 3 );
-					} elseif ( $cctor_x_days_expiration ) {
-						update_post_meta( $cctor_exp_option->post->ID, 'cctor_expiration_option', 4 );
-					}
-
-				endwhile;
-			}
-
-
-			wp_reset_postdata();
-
-			update_option( 'coupon_update_expiration_type', date( 'l jS \of F Y h:i:s A' ) );
-
-		}
-
-	/***************************************************************************/
-	/*
-	* On Update Query Coupons and Change cctor_ignore_expiration value from on to 1
-	* This if for Coupons made prior to 1.80
-	* @version 2.1
-	*/
-	public static function cctor_update_ignore_expiration() {
-
-		update_option( 'coupon_update_ignore_expiration', date( 'l jS \of F Y h:i:s A' ) );
-
-		$args = array(
-			'posts_per_page' => 1000,
-			'post_type'      => 'cctor_coupon',
-			'post_status'    => 'publish',
-			'meta_key'       => 'cctor_ignore_expiration',
-			'meta_value'     => 'on'
-		);
-
-		$cctor_ignore_exp = new WP_Query( $args );
-
-		if ( $cctor_ignore_exp ) {
-			while ( $cctor_ignore_exp->have_posts() ) : $cctor_ignore_exp->the_post();
-
-				update_post_meta( $cctor_ignore_exp->post->ID, 'cctor_ignore_expiration', 1 , 'on' );
-
-			endwhile;
-		}
-
-		wp_reset_postdata();
-
-	}
-
-
-	/***************************************************************************/
-	/*
-	* On Update Query Coupons and update cctor_outer_radius to cctor_img_outer_radius value and delete
-	* This if for Image Coupons made prior to 2.1
-	* @version 2.1
-	*/
-	public static function cctor_update_image_fields() {
-
-		update_option( 'coupon_update_image_border_meta', date( 'l jS \of F Y h:i:s A' ) );
-
-		$args = array(
-			'posts_per_page' => 1000,
-			'post_type'      => 'cctor_coupon',
-			'post_status'    => 'publish',
-			'meta_key'       => 'cctor_img_outer_radius'
-		);
-
-		$cctor_ignore_exp = new WP_Query( $args );
-
-		if ( $cctor_ignore_exp ) {
-			while ( $cctor_ignore_exp->have_posts() ) : $cctor_ignore_exp->the_post();
-
-				update_post_meta( $cctor_ignore_exp->post->ID, 'cctor_outer_radius', get_post_meta( $cctor_ignore_exp->post->ID, 'cctor_img_outer_radius', true) );
-
-				delete_post_meta( $cctor_ignore_exp->post->ID, 'cctor_img_outer_radius' );
-
-			endwhile;
-		}
-
-		wp_reset_postdata();
-
 	}
 
 	/***************************************************************************/
@@ -324,136 +196,6 @@ if ( ! class_exists( 'Coupon_Creator_Plugin_Admin' ) ) {
 				//Hook to Load New Styles and Scripts
 				do_action('cctor_meta_scripts_styles');
 
-			}
-		}
-
-		/***************************************************************************/
-
-		/*
-		* Remove Coupon Row Actions if user does not have permision to manage
-		* @version 1.90
-		* @param array $actions, $post
-		*/
-		public static function cctor_remove_coupon_row_actions( $actions, $post ) {
-		  global $current_screen, $current_user;
-
-			if( is_object( $current_screen ) && $current_screen->post_type != 'cctor_coupon' ) {
-				return $actions;
-			}
-
-			if(!current_user_can( 'edit_others_cctor_coupons', $post->ID ) && ($post->post_author != $current_user->ID))  {
-				unset( $actions['edit'] );
-				unset( $actions['view'] );
-				unset( $actions['trash'] );
-				unset( $actions['inline hide-if-no-js'] );
-			}
-
-			return $actions;
-		}
-
-
-		/***************************************************************************/
-
-		/*
-		* Setup Custom Columns
-		* @version 2.0
-		* @param array $columns
-		*/
-		public static function cctor_list_columns( $columns ) {
-			$cctor_columns = array();
-
-			if( isset( $columns['cb'] ) ) {
-				$cctor_columns['cb'] = $columns['cb'];
-			}
-
-			if( isset( $columns['title'] ) ) {
-				$cctor_columns['title'] = __( 'Coupon Title', 'coupon-creator' );
-			}
-
-			if( isset( $columns['author'] ) ) {
-				$cctor_columns['author'] = $columns['author'];
-			}
-
-			$cctor_columns['cctor_showing'] = __( 'Coupon is ', 'coupon-creator' );
-
-			$cctor_columns['cctor_shortcode'] = __( 'Shortcode', 'coupon-creator' );
-
-			$cctor_columns['cctor_ignore_expiration'] = __( 'Ignore Expiration', 'coupon-creator' );
-
-			$cctor_columns['cctor_expiration_date'] = __( 'Expiration Date', 'coupon-creator' );
-
-
-			if( isset( $columns['date'] ) ) {
-				$cctor_columns['date'] = $columns['date'];
-			}
-
-			//Filter Columns
-			if(has_filter('cctor_filter_coupon_list_columns')) {
-
-				/**
-				 * Filter the Admin Coupon List Columns Headers
-				 *
-				 * @param array $cctor_columns an array of column headers.
-				 *
-				 */
-				$cctor_columns = apply_filters('cctor_filter_coupon_list_columns', $cctor_columns,  $columns);
-			}
-
-			return $cctor_columns;
-		}
-
-		/**
-		 * Add Custom Meta Data to Columns
-		 * @since 2.0
-		 *
-		 * @param $column
-		 * @param $post_id
-		 */
-		public static function cctor_column_cases( $column, $post_id ) {
-
-			if ( class_exists( 'CCtor_Pro_Expiration_Class' ) ) {
-				$coupon_expiration = new CCtor_Pro_Expiration_Class();
-			} else {
-				$coupon_expiration = new CCtor_Expiration_Class();
-			}
-
-			switch ( $column ) {
-				case 'cctor_showing':
-
-					echo $coupon_expiration->get_admin_list_coupon_showing();
-
-					break;
-				case 'cctor_shortcode':
-
-					echo "<code>[coupon couponid='" . $post_id . "' name='" . get_the_title( $post_id ) . "']</code>";
-
-					break;
-				case 'cctor_expiration_date':
-
-					echo $coupon_expiration->get_display_expiration();
-
-					break;
-				case 'cctor_ignore_expiration':
-
-					if ( 1 == $coupon_expiration->get_expiration_option() ) {
-						echo "<p style='padding-left:40px;'>" . __( 'Yes', 'coupon-creator' ) . "</p>";
-					}
-					break;
-			}
-
-			if ( has_filter( 'cctor_filter_column_cases' ) ) {
-
-				/**
-				 * Filter the Admin Coupon List Columns Information per Coupon
-				 *
-				 * @since 1.80
-				 *
-				 * @param string $column            a string of data to display in the admin columns.
-				 * @param int    $post_id           an integer of the coupon post
-				 * @param object $coupon_expiration the expiration object.
-				 *
-				 */
-				apply_filters( 'cctor_filter_column_cases', $column, $post_id, $coupon_expiration );
 			}
 		}
 
---
+++ admin/cctor-update-scripts.php
@@ -0,0 +1,144 @@
+<?php
+//If Direct Access Kill the Script
+if( $_SERVER[ 'SCRIPT_FILENAME' ] == __FILE__ )
+	die( 'Access denied.' );
+
+
+/**
+ * Coupon Update Scripts
+ * This class run on Version Changes Only
+ *
+ * @since 2.3
+ *
+ */
+class Coupon_Update_Scripts {
+
+	function __construct() {
+
+		self::cctor_update_expiration_option();
+
+		//self::cctor_update_ignore_expiration();
+
+		self::cctor_update_image_fields();
+
+	}
+
+	/***************************************************************************/
+			/**
+			 * Update Coupons with new Expiration Options in 2.3
+			 */
+			public static function cctor_update_expiration_option() {
+				//Run this script once
+				if ( get_option( 'coupon_update_expiration_type' ) ) {
+					return;
+				}
+				$args = array(
+					'posts_per_page' => 1000,
+					'post_type'      => 'cctor_coupon',
+					'post_status'    => 'publish',
+				);
+
+				$cctor_exp_option = new WP_Query( $args );
+
+				if ( $cctor_exp_option ) {
+
+					while ( $cctor_exp_option->have_posts() ) : $cctor_exp_option->the_post();
+
+						//If there is an Expiration Option Skip this Coupon
+						$cctor_expiration_option          = get_post_meta( $cctor_exp_option->post->ID, 'cctor_expiration_option', true );
+						if ( $cctor_expiration_option ) {
+							continue;
+						}
+
+						$cctor_ignore_expiration          = get_post_meta( $cctor_exp_option->post->ID, 'cctor_ignore_expiration', true );
+						$cctor_expiration                 = get_post_meta( $cctor_exp_option->post->ID, 'cctor_expiration', true );
+						$cctor_recurring_expiration_limit = get_post_meta( $cctor_exp_option->post->ID, 'cctor_recurring_expiration_limit', true );
+						$cctor_recurring_expiration       = get_post_meta( $cctor_exp_option->post->ID, 'cctor_recurring_expiration', true );
+						$cctor_x_days_expiration          = get_post_meta( $cctor_exp_option->post->ID, 'cctor_x_days_expiration', true );
+
+						if ( 1 == $cctor_ignore_expiration ) {
+							update_post_meta( $cctor_exp_option->post->ID, 'cctor_expiration_option', 1 );
+						} elseif ( $cctor_expiration && ! $cctor_recurring_expiration_limit && ! $cctor_recurring_expiration ) {
+							update_post_meta( $cctor_exp_option->post->ID, 'cctor_expiration_option', 2 );
+						} elseif ( $cctor_expiration && $cctor_recurring_expiration_limit && $cctor_recurring_expiration ) {
+							update_post_meta( $cctor_exp_option->post->ID, 'cctor_expiration_option', 3 );
+						} elseif ( $cctor_x_days_expiration ) {
+							update_post_meta( $cctor_exp_option->post->ID, 'cctor_expiration_option', 4 );
+						}
+
+					endwhile;
+				}
+
+
+				wp_reset_postdata();
+
+				update_option( 'coupon_update_expiration_type', date( 'l jS \of F Y h:i:s A' ) );
+
+			}
+
+		/***************************************************************************/
+		/*
+		* On Update Query Coupons and Change cctor_ignore_expiration value from on to 1
+		* This if for Coupons made prior to 1.80
+		* @version 2.1
+		*/
+		public static function cctor_update_ignore_expiration() {
+
+			update_option( 'coupon_update_ignore_expiration', date( 'l jS \of F Y h:i:s A' ) );
+
+			$args = array(
+				'posts_per_page' => 1000,
+				'post_type'      => 'cctor_coupon',
+				'post_status'    => 'publish',
+				'meta_key'       => 'cctor_ignore_expiration',
+				'meta_value'     => 'on'
+			);
+
+			$cctor_ignore_exp = new WP_Query( $args );
+
+			if ( $cctor_ignore_exp ) {
+				while ( $cctor_ignore_exp->have_posts() ) : $cctor_ignore_exp->the_post();
+
+					update_post_meta( $cctor_ignore_exp->post->ID, 'cctor_ignore_expiration', 1 , 'on' );
+
+				endwhile;
+			}
+
+			wp_reset_postdata();
+
+		}
+
+
+		/***************************************************************************/
+		/*
+		* On Update Query Coupons and update cctor_outer_radius to cctor_img_outer_radius value and delete
+		* This if for Image Coupons made prior to 2.1
+		* @version 2.1
+		*/
+		public static function cctor_update_image_fields() {
+
+			update_option( 'coupon_update_image_border_meta', date( 'l jS \of F Y h:i:s A' ) );
+
+			$args = array(
+				'posts_per_page' => 1000,
+				'post_type'      => 'cctor_coupon',
+				'post_status'    => 'publish',
+				'meta_key'       => 'cctor_img_outer_radius'
+			);
+
+			$cctor_ignore_exp = new WP_Query( $args );
+
+			if ( $cctor_ignore_exp ) {
+				while ( $cctor_ignore_exp->have_posts() ) : $cctor_ignore_exp->the_post();
+
+					update_post_meta( $cctor_ignore_exp->post->ID, 'cctor_outer_radius', get_post_meta( $cctor_ignore_exp->post->ID, 'cctor_img_outer_radius', true) );
+
+					delete_post_meta( $cctor_ignore_exp->post->ID, 'cctor_img_outer_radius' );
+
+				endwhile;
+			}
+
+			wp_reset_postdata();
+
+		}
+}
