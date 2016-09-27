d9afe9df8bbf6a64ba2c04afa2d2fc838dcb773c
Brian Jessee <info@jesseeproductions.com>
2016-09-27 10:11:25 -0400

    Refactor Pro Cont - Styling Changes and Tab Script Enabled for Multiple Versions

--- src/resources/js/coupon-admin.js
+++ src/resources/js/coupon-admin.js
@@ -4,14 +4,9 @@
 (function ( $ ) {
 
 	//Init Main Tabs
-	//pngx_admin_tabs.init( '.main.pngx-tabs' );
-
-	var main_tabs;
-	$( window ).on( 'load', function ( e ) {
-		console.log( 'here' );
-		main_tabs = pngx_admin_tabs;
-		main_tabs.init( '.main.pngx-tabs' );
-	} );
+	var pngx_main_tabs = pngx_main_tabs || {};
+	var main_tabs = new Pngx_Admin_Tabs( jQuery, pngx_main_tabs );
+	main_tabs.init('.main.pngx-tabs');
 
 	//Create Image Upload Object
 	var coupon_img = new PNGX__Media( $, 'input#cctor_image', 'Choose Coupon Image', 'Use Image' );
