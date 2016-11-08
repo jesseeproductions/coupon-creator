83ce6fa6b6d191d309e19fd47c3c57ac750180dd
Brian Jessee <info@jesseeproductions.com>
2016-11-08 09:26:48 -0500

    Remove Deleted Files from merge

--- includes/deprecated/Deprecated.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: brianjessee
- * Date: 6/7/2016
- * Time: 9:13 AM
- */
--- src/deprecated/Coupon_Creator_plugin.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: brianjessee
- * Date: 8/31/2016
- * Time: 10:49 AM
- */
