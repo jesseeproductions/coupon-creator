4c401e90cc1f376b2d13559893f6bb45c4811b47
brian <info@jesseeproductions.com>
2016-11-08 09:21:31 -0500

    merge conflict fixes for 2.4

---
+++ includes/deprecated/Deprecated.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brianjessee
+ * Date: 6/7/2016
+ * Time: 9:13 AM
+ */
---
+++ src/deprecated/Coupon_Creator_plugin.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brianjessee
+ * Date: 8/31/2016
+ * Time: 10:49 AM
+ */
