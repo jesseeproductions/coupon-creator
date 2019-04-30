564dc7b03f9529fa4ba7c0c534281ad5b4e4e931
jesseeproductions <info@jesseeproductions.com>
2019-04-30 10:12:19 -0400

    update icon styling and display in admin menu

--- src/Cctor/Post_Type_Coupon.php
+++ src/Cctor/Post_Type_Coupon.php
@@ -221,7 +221,40 @@ class Cctor__Coupon__Post_Type_Coupon {
 				$this->text_domain,
 				array(
 					'supports'  => array( 'title', 'coupon_creator_meta_box' ),
-					'menu_icon' => pngx( 'cctor' )->resource_url . 'images/cctor-icon.svg',
+					//'menu_icon' => 'data:image/svg+xml;base64,' . base64_encode('<svg width="20" height="20" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path fill="black" d="M1591 1448q56 89 21.5 152.5t-140.5 63.5h-1152q-106 0-140.5-63.5t21.5-152.5l503-793v-399h-64q-26 0-45-19t-19-45 19-45 45-19h512q26 0 45 19t19 45-19 45-45 19h-64v399zm-779-725l-272 429h712l-272-429-20-31v-436h-128v436z"/></svg>')
+					'menu_icon' => 'data:image/svg+xml;base64,' . base64_encode('
+					<svg
+						version="1.1"
+						id="coupon-creator-icon"
+						xmlns="http://www.w3.org/2000/svg"
+						xmlns:xlink="http://www.w3.org/1999/xlink"
+						x="0px" 
+						y="0px"
+						viewBox="0 0 24 24"
+						width="20px" 
+						height="20px"
+					>
+					<g fill="black">
+						<g>
+							<g >
+								<polygon points="21,21 17,21 17,19 19,19 19,17 21,17 			"/>
+								<rect x="9.3" y="19" width="5.3" height="2"/>
+								<polygon points="7,21 3,21 3,17 5,17 5,19 7,19 			"/>
+								<rect x="3" y="9.3" width="2" height="5.3"/>
+								<polygon points="5,7 3,7 3,3 7,3 7,5 5,5 			"/>
+								<rect x="9.3" y="3" width="5.3" height="2"/>
+								<polygon points="21,7 19,7 19,5 17,5 17,3 21,3 			"/>
+								<rect x="19" y="9.3" width="2" height="5.3"/>
+							</g>
+						</g>
+						<g>
+							<path fill="black" d="M10.1,15.6c-0.4-0.5-0.6-1.2-0.6-2.1v-3c0-0.9,0.2-1.6,0.6-2.1c0.4-0.5,1-0.7,1.9-0.7c0.9,0,1.5,0.2,1.9,0.6
+								c0.4,0.4,0.5,1,0.5,1.8v0.7h-1.8V10c0-0.3,0-0.6-0.1-0.7C12.5,9.1,12.3,9,12.1,9c-0.3,0-0.4,0.1-0.5,0.3c-0.1,0.2-0.1,0.4-0.1,0.8
+								V14c0,0.3,0,0.6,0.1,0.8c0.1,0.2,0.3,0.3,0.5,0.3c0.3,0,0.4-0.1,0.5-0.3c0.1-0.2,0.1-0.4,0.1-0.8v-0.9h1.8v0.7
+								c0,0.8-0.2,1.4-0.5,1.8c-0.4,0.4-1,0.7-1.9,0.7C11.1,16.3,10.5,16.1,10.1,15.6z"/>
+						</g>
+					</g>
+					</svg>')
 				)
 			);
 
