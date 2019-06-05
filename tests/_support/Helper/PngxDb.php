53b056f737f8e1dbc999a91e720debe41a5fd99d
jesseeproductions <info@jesseeproductions.com>
2019-06-05 12:34:46 -0400

    add tests for link attributes

---
+++ tests/_support/Helper/PngxDb.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Helper;
+
+use Codeception\Module\WPDb;
+
+class PngxDB extends \Codeception\Module {
+
+	/**
+	 * @var WPDb
+	 */
+	protected $db;
+
+	public function _initialize() {
+		$this->db = $this->getModule( 'WPDb' );
+	}
+
+	/**
+	 * Gets an option from the tribe option database row.
+	 *
+	 * @param string $key
+	 * @param mixed  $default
+	 *
+	 * @return mixed
+	 */
+	public function getCouponOptionFromDatabase( $key, $default = '' ) {
+		$options = $this->db->grabOptionFromDatabase( 'coupon_creator_options' );
+		if ( empty( $options ) ) {
+			return $default;
+		}
+
+		return isset( $options[ $key ] ) ? $options[ $key ] : $default;
+	}
+
+	/**
+	 * Sets an option in the tribe option row.
+	 *
+	 * @param string       $key
+	 * @param string|array $value
+	 */
+	public function setCouponOption( $key, $value ) {
+		$option_name = 'coupon_creator_options';
+		$options     = $this->db->grabOptionFromDatabase( $option_name );
+		if ( empty( $options ) ) {
+			$this->db->haveOptionInDatabase( $option_name, [ $key => $value ] );
+		} else {
+			$this->db->haveOptionInDatabase( $option_name, array_merge( $options, [ $key => $value ] ) );
+		}
+	}
+
+	/**
+	 * Inserts many coupons in the database.
+	 *
+	 * @param int   $count         The number of coupons to insert.
+	 * @param array $overrides     An array of arguments to override the defaults (see `haveCouponInDatabase`)
+	 *
+	 * @return array An array of generated coupon post IDs.
+	 */
+	public function haveManyCouponsInDatabase( $count, array $overrides = [] ) {
+		$ids  = [];
+		for ( $n = 0; $n < $count; $n ++ ) {
+			$coupon_overrides = $overrides;
+			$ids[] = $this->haveCouponInDatabase( $coupon_overrides );
+		}
+
+		return $ids;
+	}
+
+	/**
+	 * Inserts an coupon in the database.
+	 *
+	 * @param array $overrides An array of values to override the default arguments.
+	 *                         Keep in mind `tax_input` and `meta_input` to bake terms and custom fields in.
+	 *
+	 * @return int The generated coupon post ID
+	 */
+	public function haveCouponInDatabase( array $overrides = [] ) {
+
+		$prefix           = 'cctor_';
+		$period           = rand( 1, 5 );
+		$id               = uniqid( mt_rand( 1, 999 ), true );
+		$expiration       = date( 'm/d/Y', strtotime( '+' . $period . ' months' ) );
+		$expiration_mysql = date( 'Y-m-d h:i:s', strtotime( $expiration ) );
+
+		$meta_input = [
+			$prefix . 'coupon_type'       => 'default', //image
+			$prefix . 'expiration_option' => 2, //1
+			$prefix . 'date_format'       => 1, // 0
+			$prefix . 'expiration'        => $expiration,
+			$prefix . 'expiration_mysql'  => $expiration_mysql,
+			$prefix . 'amount'            => "Coupon {$id}",
+			$prefix . 'colorheader'       => '#' . substr( md5( mt_rand() ), 0, 6 ),
+			$prefix . 'colordiscount'     => '#' . substr( md5( mt_rand() ), 0, 6 ),
+			$prefix . 'description'       => "Coupon Terms {$id}",
+			$prefix . 'bordercolor'       => '#' . substr( md5( mt_rand() ), 0, 6 ),
+			//$prefix . 'image'     => 10, // id 10
+		];
+
+		$tax_input = [];
+		if ( isset( $overrides['categories'] ) ) {
+			$tax_input['cctor_coupon_category'] = (array) $overrides['categories'];
+			unset( $overrides['categories'] );
+		}
+
+		if ( ! empty( $tax_input ) ) {
+			if ( isset( $overrides['tax_input'] ) ) {
+				$overrides['tax_input'] = array_merge( $overrides['tax_input'], $tax_input );
+			} else {
+				$overrides['tax_input'] = $tax_input;
+			}
+		}
+
+		$defaults = [
+			'post_type'  => 'cctor_coupon',
+			'post_title' => "Coupon {$id}",
+			'post_name'  => "coupon-{$id}",
+			'meta_input' => isset( $overrides['meta_input'] ) ? array_merge( $meta_input, $overrides['meta_input'] ) : $meta_input,
+		];
+
+		unset( $overrides['meta_input'] );
+
+		$post_id = $this->db->havePostInDatabase( array_merge( $defaults, $overrides ) );
+
+		return $post_id;
+	}
+
+}
--- tests/acceptance/Options/OptionsCest.php
+++ tests/acceptance/Options/OptionsCest.php
@@ -11,7 +11,7 @@ class OptionsCest extends BaseAcceptanceCest {
 	 * @test
 	 * since TBD
 	 */
-	public function should_have_option_fields_and_updated_message( AcceptanceTester $I ) {
+/*	public function should_have_option_fields_and_updated_message( AcceptanceTester $I ) {
 
 		$I->loginAsAdmin();
 
@@ -22,13 +22,13 @@ class OptionsCest extends BaseAcceptanceCest {
 		$I->click( '.submit .button-primary' );
 		$I->seeInSource( '<div class="updated fade"><p>Coupon Creator Options updated.</p></div>' );
 
-	}
+	}*/
 
 	/**
 	 * @test
 	 * since TBD
 	 */
-	public function should_have_default_options_in_new_coupon( AcceptanceTester $I ) {
+/*	public function should_have_default_options_in_new_coupon( AcceptanceTester $I ) {
 
 		$I->loginAsAdmin();
 		$I->amOnAdminPage( '/edit.php?post_type=cctor_coupon&page=coupon-options' );
@@ -39,7 +39,7 @@ class OptionsCest extends BaseAcceptanceCest {
 		$I->selectOption( '#cctor_default_date_format', 'Day First - DD/MM/YYYY' );
 
 		// show the colorpicker input fields
-		$I->executeJS('$(".wp-picker-input-wrap").show();');
+		$I->executeJS( '$(".wp-picker-input-wrap").show();' );
 		$I->fillField( '#cctor_border_color', '#b02f30' );
 		$I->fillField( '#cctor_discount_bg_color', '#2a5491' );
 		$I->fillField( '#cctor_discount_text_color', '#2dad6b' );
@@ -49,11 +49,11 @@ class OptionsCest extends BaseAcceptanceCest {
 
 		// start check of defaults on add new coupon
 		$I->amOnAdminPage( '/post-new.php?post_type=cctor_coupon' );
-		$I->wait(2);
+		$I->wait( 2 );
 
 		// maximize window to see tabs
 		$I->maximizeWindow();
-		$I->executeJS('$(".wp-picker-input-wrap").show();');
+		$I->executeJS( '$(".wp-picker-input-wrap").show();' );
 
 		$I->seeInField( '#cctor_colordiscount', '#2a5491' );
 		$I->seeInField( '#cctor_colorheader', '#2dad6b' );
@@ -73,5 +73,90 @@ class OptionsCest extends BaseAcceptanceCest {
 
 		$I->click( '#ui-id-5' );
 		$I->seeElement( '.pngx-tab-heading-help' );
+	}*/
+
+	/**
+	 * @test
+	 * since TBD
+	 */
+	public function should_change_nofollow_and_noindex_in_print_view( AcceptanceTester $I ) {
+		$name      = 'coupon-links-01';
+		$coupon_id = $I->haveCouponInDatabase( [
+			'post_title' => 'Coupon Links 01',
+			'post_name'  => $name,
+		] );
+		$I->amOnPage( '/cctor_coupon/' . $name . '/' );
+		$I->makeScreenshot();
+		$I->canSeeInPageSource('<meta name="robots" content="noindex,nofollow"/>');
+		$I->haveOptionInDatabase( $this->option_name, [ 'cctor_nofollow_print_template' => false ] );
+		$I->reloadPage();
+		$I->dontSeeInPageSource('<meta name="robots" content="noindex,nofollow"/>');
 	}
+
+	/**
+	 * @test
+	 * since TBD
+	 */
+/*	public function should_change_coupon_permalink( AcceptanceTester $I ) {
+		$I->haveOptionInDatabase( $this->option_name, [ 'cctor_coupon_base' => 'coupon' ] );
+		$name      = 'coupon-links-02';
+		$coupon_id = $I->haveCouponInDatabase( [
+			'post_title' => 'Coupon Links 02',
+			'post_name'  => $name,
+		] );
+		$I->wait(5);
+		$I->amOnPage( '/coupon/' . $name . '/' );
+		$I->makeScreenshot();
+		$I->canSeeInPageSource('<meta name="robots" content="noindex,nofollow"/>');
+	}*/
+
+	/**
+	 * @test
+	 * since TBD
+	 */
+/*	public function should_change_nofollow_in_shortcode_view( AcceptanceTester $I ) {
+		$name      = 'coupon-links-03';
+		$coupon_id = $I->haveCouponInDatabase( [
+			'post_title' => 'Coupon Links 03',
+			'post_name'  => $name,
+		] );
+
+		// add shortcode to a page
+		$coupons_page = $I->havePageInDatabase( [
+			'post_title'   => 'Coupons',
+			'post_name'    => 'coupons',
+			'post_content' => '[coupon couponid="'.$coupon_id.'" coupon_align="cctor_alignnone" name="'.$name.'"]',
+		] );
+		$I->amOnPage( '/coupons/' );
+		$I->canSeeInPageSource('class="print-link" rel="nofollow"');
+		$I->haveOptionInDatabase( $this->option_name, [ 'cctor_nofollow_print_link' => false ] );
+		$I->reloadPage();
+		$I->dontSeeInPageSource( 'class="print-link" rel="nofollow"' );
+
+	}*/
+
+	/**
+	 * @test
+	 * since TBD
+	 */
+/*	public function should_change_disable_print_view( AcceptanceTester $I ) {
+		$name      = 'coupon-links-04';
+		$coupon_id = $I->haveCouponInDatabase( [
+			'post_title' => 'Coupon Links 04',
+			'post_name'  => $name,
+		] );
+
+		// add shortcode to a page
+		$coupons_page = $I->havePageInDatabase( [
+			'post_title'   => 'Coupons',
+			'post_name'    => 'coupons',
+			'post_content' => '[coupon couponid="'.$coupon_id.'" coupon_align="cctor_alignnone" name="'.$name.'"]',
+		] );
+		$I->amOnPage( '/coupons/' );
+		$I->SeeElement( '.print-link' );
+		$I->haveOptionInDatabase( $this->option_name, [ 'cctor_hide_print_link' => true ] );
+		$I->reloadPage();
+		$I->dontSeeElement( '.print-link' );
+
+	}*/
 }
