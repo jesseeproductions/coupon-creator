640b984e558a9a767db50b3575d3f18f1f15d0d7
jesseeproductions <info@jesseeproductions.com>
2023-05-12 10:43:08 -0400

    update test fields to v2 version

--- src/Cctor/Test/Meta_Fields.php
+++ src/Cctor/Test/Meta_Fields.php
@@ -37,116 +37,139 @@ class Meta_Fields {
 	 * @return array<string|mixed> $fields The fields.
 	 */
 	public function get_fields( array $fields = [] ) {
-		$post_id = get_the_ID();
+		$post_id    = get_the_ID();
 		$post_types = [
-		    [
-		        "text" => "Posts",
-		        "id" => "post",
-		        "selected" => true
-		    ],
-		    [
-		        "text" => "Pages",
-		        "id" => "page",
-		        "selected" => false
-		    ],
-		    [
-		        "text" => "Products",
-		        "id" => "product",
-		        "selected" => false
-		    ],
-		    [
-		        "text" => "Coupons",
-		        "id" => "cctor_coupon",
-		        "selected" => false
-		    ]
+			[
+				"text"     => "Posts",
+				"id"       => "post",
+				"selected" => true
+			],
+			[
+				"text"     => "Pages",
+				"id"       => "page",
+				"selected" => false
+			],
+			[
+				"text"     => "Products",
+				"id"       => "product",
+				"selected" => false
+			],
+			[
+				"text"     => "Coupons",
+				"id"       => "cctor_coupon",
+				"selected" => false
+			]
 		];
 
 		$fields[ $this->fields_prefix . 'content_source' ] = [
-			'class'       => 'large',
-			'label'       => __( 'Content Source', 'coupon-test' ),
-			'desc'        => '',
-			'id'          => $this->fields_prefix . 'content_source',
-			'value'       => 'post',
-			'type'        => 'wooselect',
-			'options'     => $post_types,
-			'attrs'       => [
+			'label'         => __( 'Content Source', 'coupon-test' ),
+			'tooltip'       => __( 'Tooltip', 'test' ),
+			'id'            => $this->fields_prefix . 'content_source',
+			'version'       => 'v2',
+			'value'         => 'post',
+			'type'          => 'wooselect',
+			'options'       => $post_types,
+			'attrs'         => [
+				'data-placeholder'        => _x( 'Select a content source.', 'The placeholder for a dropdown.', 'coupon-test' ),
+				'data-prevent-clear'      => true,
+				'data-force-search'       => false,
+				'data-dropdown-css-width' => '0',
+				'style'                   => 'width: 100%;',
+			],
+			'section'       => 'coupon_creator_meta_box',
+			'tab'           => 'test',
+			'fieldset_wrap' => [ 'cctor-coupon-test-content-select-field' ],
+			'priority'      => 0.01,
+		];
+
+		$fields[ $this->fields_prefix . 'multiple' ] = [
+			'label'         => __( 'multiple Source', 'coupon-test' ),
+			'tooltip'       => __( 'Tooltip', 'test' ),
+			'id'            => $this->fields_prefix . 'multiple',
+			'version'       => 'v2',
+			'value'         => 'post',
+			'type'          => 'wooselect',
+			'options'       => $post_types,
+			'attrs'         => [
 				'data-placeholder'        => _x( 'Select a content source.', 'The placeholder for a dropdown.', 'coupon-test' ),
 				'data-prevent-clear'      => true,
 				'data-force-search'       => true,
 				'data-dropdown-css-width' => '0',
 				'style'                   => 'width: 100%;',
+				'multiple'                => true,
 			],
-			'section'      => 'coupon_creator_meta_box',
-			'tab'          => 'test',
+			'section'       => 'coupon_creator_meta_box',
+			'tab'           => 'test',
 			'fieldset_wrap' => [ 'cctor-coupon-test-content-select-field' ],
-			'priority'     => 0.01,
+			'priority'      => 0.01,
 		];
 
 		$fields[ $this->fields_prefix . 'test_actions' ] = [
-			'label'   => '',
-			'desc'    => '',
-			'id'      => $this->fields_prefix . 'test_actions',
-			'class'   => 'cctor-coupon-test-field',
-			'type'    => 'test_actions',
-			'section' => 'coupon_creator_meta_box',
-			'tab'     => 'test',
+			'label'    => '',
+			'desc'     => '',
+			'id'       => $this->fields_prefix . 'test_actions',
+			'version'  => 'v2',
+			'class'    => 'cctor-coupon-test-field',
+			'type'     => 'test_actions',
+			'section'  => 'coupon_creator_meta_box',
+			'tab'      => 'test',
 			'priority' => 0.01,
 		];
 
-/*		$fields[ $this->fields_prefix . 'tokens' ] = [
-			'label'   => __( 'Tokens', 'coupon-test' ),
-			'desc'    => '',
-			'id'      => $this->fields_prefix . 'tokens',
-			'data'    => [],
-			'value'    => '',
-			'class'   => 'cctor-coupon-test-field',
-			'type'    => 'read-only',
-			'section' => 'coupon_creator_meta_box',
-			'tab'     => 'test',
+		$fields[ $this->fields_prefix . 'textinput' ] = [
+			'label'    => __( 'Text Label', 'test' ),
+			'tooltip'  => __( 'Tooltip', 'test' ),
+			'id'       => $this->fields_prefix . 'textinput',
+			'version'  => 'v2',
+			'type'     => 'text',
+			'section'  => 'coupon_creator_meta_box',
+			'tab'      => 'test',
 			'priority' => 0.01,
-		];*/
-
-/*		$fields[ $this->fields_prefix . 'test_profile' ] = [
-			'class'         => 'large',
-			'label'         => __( 'Test Profile', 'coupon-test' ),
-			'desc'          => '',
-			'id'            => $this->fields_prefix . 'test_profile',
-			'value'         => '',
-			'type'          => 'wooselect',
-			'options'       => [],
-			'attrs'         => [
-				'data-placeholder'        => _x( 'Select test profile.', 'The placeholder for a dropdown.', 'coupon-test' ),
-				'data-prevent-clear'      => true,
-				'data-force-search'       => true,
-				'data-dropdown-css-width' => '0',
-				'style'                   => 'width: 100%;',
-			],
-			'section'       => 'coupon_creator_meta_box',
-			'tab'           => 'test',
-			'fieldset_wrap' => [ 'cctor-coupon-test-content-select-field' ],
-			'priority'      => 0.01,
 		];
 
-		$fields[ $this->fields_prefix . 'profile_id' ] = [
-			'class'         => 'large',
-			'label'         => __( 'Profile.', 'coupon-test' ),
-			'desc'          => '',
-			'id'            => $this->fields_prefix . 'profile_id',
-			'value'         => '',
-			'type'          => 'wooselect',
-			'options'       => [],
-			'attrs'         => [
-				'data-placeholder'        => _x( 'Select a profile.', 'The placeholder for a dropdown.', 'coupon-test' ),
-				'data-prevent-clear'      => true,
-				'data-force-search'       => true,
-				'data-dropdown-css-width' => '0',
-				'style'                   => 'width: 100%;',
-			],
-			'section'       => 'coupon_creator_meta_box',
-			'tab'           => 'test',
-			'fieldset_wrap' => [ 'cctor-coupon-test-content-select-field' ],
-			'priority'      => 0.01,
-		];*/
+		$fields[ $this->fields_prefix . 'textarea' ] = [
+			'label'    => __( 'Textarea Label', 'test' ),
+			'tooltip'  => __( 'Tooltip', 'test' ),
+			'id'       => $this->fields_prefix . 'textarea',
+			'version'  => 'v2',
+			'type'     => 'textarea',
+			'section'  => 'coupon_creator_meta_box',
+			'tab'      => 'test',
+			'priority' => 0.01,
+		];
+
+		$fields[ $this->fields_prefix . 'number' ] = [
+			'label'    => __( 'Number Label', 'test' ),
+			'tooltip'  => __( 'Tooltip', 'test' ),
+			'id'       => $this->fields_prefix . 'number',
+			'version'  => 'v2',
+			'type'     => 'number',
+			'section'  => 'coupon_creator_meta_box',
+			'tab'      => 'test',
+			'priority' => 0.01,
+		];
+
+		$fields[ $this->fields_prefix . 'switchinput' ] = [
+			'label'    => __( 'Switch Label', 'test' ),
+			'tooltip'  => __( 'Tooltip', 'test' ),
+			'id'       => $this->fields_prefix . 'switchinput',
+			'version'  => 'v2',
+			'type'     => 'switch',
+			'section'  => 'coupon_creator_meta_box',
+			'tab'      => 'test',
+			'priority' => 0.01,
+		];
+
+		$fields[ $this->fields_prefix . 'icon' ] = [
+			'label'    => __( 'Icon', 'test_actions' ),
+			'id'       => $this->fields_prefix . 'icon',
+			'version'  => 'v2',
+			'type'     => 'image',
+			'imagemsg' => 'Icon',
+			'section'  => 'coupon_creator_meta_box',
+			'tab'      => 'test',
+			'priority' => 0.01,
+		];
 
 		return $fields;
 	}
